be7335d5ef8011b1b9d1d5d88307767144a7fe49
Sahil Kumbharkar <sahilkumbharkar57@gmail.com>
2025-07-21 23:56:10 +0530

    Fourth commit

--- Client/Components/Chat.jsx
+++ Client/Components/Chat.jsx
@@ -1,7 +1,6 @@
 
 
 function Chat({ sender, msg, image }) {
-    console.log("Sent message:", msg, image);
     return (
         <div className={`flex flex-col  gap-2 py-1 px-3 ${sender === 'You' ? 'items-end' : 'items-start'}`}>
             {image && <div className=' border rounded-xl max-w-[50%]'>
